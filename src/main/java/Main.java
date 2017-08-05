2ad5c9f4e9db582debccb1e9c1f4a7ebf1d7d9cb
Pierre Renaux <pierre@talansoft.com>
2017-08-05 21:12:09 +0800

    feat(Main.java): Concurrent solving ;
    “Benchmark” of the concurrent solver, with solution printing disabled:

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,53 @@
+import jdk.nashorn.internal.codegen.CompilerConstants;
+
+import java.util.ArrayList;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) {
+        final int threads = Runtime.getRuntime().availableProcessors();
         final int N = 8;
+        final boolean printSolutions = true;
 
-        System.out.println("Hello Queens!");
+        System.out.println("# Using " + threads + " threads.");
+        ExecutorService exec = Executors.newScheduledThreadPool(threads);
+
         System.out.println("Solving for N = " + N);
 
-        final Solver solver = new Solver(N);
-        int numSolutions = 0;
-        while (solver.solve()) {
-            System.out.println("# Solution " + (numSolutions+1) + "\n" + solver.toString(true));
-            ++numSolutions;
+        ArrayList<Future<ArrayList<String>>> futureSolutions = new ArrayList<Future<ArrayList<String>>>();
+
+        for (int i = 0; i < N; ++i) {
+            final int row = i;
+            futureSolutions.add(exec.submit(new Callable<ArrayList<String>>() {
+                public ArrayList<String> call() {
+                    ArrayList<String> sols = new ArrayList<String>();
+                    Solver solver = new Solver(N);
+                    solver.startRow(row);
+                    while (solver.solveRow(row)) {
+                        sols.add(solver.toString(true));
+                    }
+                    return sols;
+                }
+            }));
         }
-        System.out.println("# Num iterations: " + solver.getTotalIt());
+
+        int numSolutions = 0;
+        for (int i = 0; i < N; ++i) {
+            try {
+                ArrayList<String> r = futureSolutions.get(i).get();
+                for (String sol : r) {
+                    if (printSolutions) {
+                        System.out.println("# Solution " + (numSolutions + 1) + "\n" + sol);
+                    }
+                    ++numSolutions;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
         System.out.println("# Num solutions: " + numSolutions);
+
+        exec.shutdown();
     }
 }
