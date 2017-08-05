88db17fbf83e38c52db1bf6fdfabfe1ec91aee52
Pierre Renaux <pierre@talansoft.com>
2017-08-06 02:57:43 +0800

    feat(Main.java): Use Akka to handle the solutions as they are generated and don't store all of them in memory, this should allow any N that don't take a century to compute to output all of the solutions without running out of memory ;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,84 +1,70 @@
-import jdk.nashorn.internal.codegen.CompilerConstants;
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
 
 import java.util.ArrayList;
 import java.util.concurrent.*;
 
-public class Main {
+public class Main extends AbstractActor {
+
     public static void main(String[] args) {
         final int threads = Runtime.getRuntime().availableProcessors();
         final int N = 8;
         // Don't print solutions above N=10-ish since they are so many that
-        // most of the time spent is to print. Also the current dumb-ass
-        // concurrent implementation stores all the results in memory before
-        // printing so memory will run out for N=16+.
-        final boolean printSolutions = true;
+        // most of the time spent is to print.
+        final boolean printSolutions = (N <= 12) ? true : false;
 
         System.out.println("# Using " + threads + " threads.");
         ExecutorService exec = Executors.newScheduledThreadPool(threads);
 
         System.out.println("Solving for N = " + N);
 
-        int numSolutions = 0;
-        if (printSolutions) {
-            ArrayList<Future<ArrayList<String>>> futureSolutions = new ArrayList<Future<ArrayList<String>>>();
+        final ActorSystem actorSystem = ActorSystem.create("QueensSolutionsReceiver");
+        final ActorRef solutionHandler = actorSystem.actorOf(SolutionHandler.props(), "SolutionsHandler");
 
-            for (int i = 0; i < N; ++i) {
-                final int row = i;
-                futureSolutions.add(exec.submit(new Callable<ArrayList<String>>() {
-                    public ArrayList<String> call() {
-                        ArrayList<String> sols = new ArrayList<String>();
-                        Solver solver = new Solver(N);
-                        solver.startRow(row);
-                        while (solver.solveRow(row)) {
-                            sols.add(solver.toString(true));
-                        }
-                        return sols;
-                    }
-                }));
-            }
+        ArrayList<Future<Integer>> futureSolutions = new ArrayList<Future<Integer>>();
 
-            for (int i = 0; i < N; ++i) {
-                try {
-                    ArrayList<String> r = futureSolutions.get(i).get();
-                    for (String sol : r) {
+        for (int i = 0; i < N; ++i) {
+            final int row = i;
+            futureSolutions.add(exec.submit(new Callable<Integer>() {
+                public Integer call() {
+                    int numSols = 0;
+                    Solver solver = new Solver(N);
+                    solver.startRow(row);
+                    while (solver.solveRow(row)) {
                         if (printSolutions) {
-                            System.out.println("# Solution " + (numSolutions + 1) + "\n" + sol);
+                            String solText = solver.toString(true);
+                            solutionHandler.tell(new SolutionHandler.MsgPrintSolution(solText), ActorRef.noSender());
                         }
-                        ++numSolutions;
+                        ++numSols;
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    solutionHandler.tell(new SolutionHandler.MsgSolverDone(numSols, solver.getTotalIt()), ActorRef.noSender());
+                    return numSols;
                 }
-            }
-        } else {
-            ArrayList<Future<Integer>> futureSolutions = new ArrayList<Future<Integer>>();
-
-            for (int i = 0; i < N; ++i) {
-                final int row = i;
-                futureSolutions.add(exec.submit(new Callable<Integer>() {
-                    public Integer call() {
-                        int numSols = 0;
-                        Solver solver = new Solver(N);
-                        solver.startRow(row);
-                        while (solver.solveRow(row)) {
-                            ++numSols;
-                        }
-                        return numSols;
-                    }
-                }));
-            }
-
-            for (int i = 0; i < N; ++i) {
-                try {
-                    numSolutions += futureSolutions.get(i).get();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+            }));
         }
 
-        System.out.println("# Num solutions: " + numSolutions);
+        for (int i = 0; i < N; ++i) {
+            try {
+                futureSolutions.get(i).get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        solutionHandler.tell(new SolutionHandler.MsgFoundAll(), ActorRef.noSender());
 
-        exec.shutdown();
+        try {
+            SolutionHandler.waitDone();
+            actorSystem.terminate();
+            exec.shutdown();
+            exec.awaitTermination(10, TimeUnit.HOURS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Receive createReceive() {
+        return null;
     }
 }
---
+++ src/main/java/SolutionHandler.java
@@ -0,0 +1,78 @@
+import akka.actor.AbstractActor;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.japi.Creator;
+import akka.japi.pf.FI;
+import org.jdeferred.impl.DeferredObject;
+
+public class SolutionHandler extends AbstractActor {
+    static public Props props() {
+        return Props.create(SolutionHandler.class,
+                new Creator<SolutionHandler>() {
+                    @Override
+                    public SolutionHandler create() throws Exception {
+                        return new SolutionHandler();
+                    }
+                }
+        );
+    }
+
+    static public class MsgPrintSolution {
+        public final String text;
+
+        public MsgPrintSolution(String aText) {
+            text = aText;
+        }
+    }
+
+    static public class MsgSolverDone {
+        public final int numSolutions;
+        public final int numIt;
+
+        public MsgSolverDone(int aNumSolutions, int aNumIt) {
+            numSolutions = aNumSolutions;
+            numIt = aNumIt;
+        }
+    }
+
+    static public class MsgFoundAll {
+    }
+
+    private int numPrintedSolutions = 0;
+    private int numSolutions = 0;
+    private int numIt = 0;
+
+    static private DeferredObject isDone = new DeferredObject();
+    static public void waitDone() throws InterruptedException {
+        isDone.waitSafely();
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(MsgPrintSolution.class, new FI.UnitApply<MsgPrintSolution>() {
+                    @Override
+                    public void apply(MsgPrintSolution aMsg) throws Exception {
+                        ++numPrintedSolutions;
+                        System.out.println("\n# Solution " + numPrintedSolutions + "\n" + aMsg.text);
+                    }
+                })
+                .match(MsgSolverDone.class, new FI.UnitApply<MsgSolverDone>() {
+                    @Override
+                    public void apply(MsgSolverDone aMsg) throws Exception {
+                        numSolutions += aMsg.numSolutions;
+                        numIt += aMsg.numIt;
+                    }
+                })
+                .match(MsgFoundAll.class, new FI.UnitApply<MsgFoundAll>() {
+                    @Override
+                    public void apply(MsgFoundAll aMsg) throws Exception {
+                        System.out.println("\n# Num iterations: " + numIt);
+                        System.out.println("# Num solutions: " + numSolutions);
+                        isDone.resolve(true);
+                    }
+                })
+                .build();
+    }
+}
